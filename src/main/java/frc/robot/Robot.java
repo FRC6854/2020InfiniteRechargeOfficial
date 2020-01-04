78fd1a9e14841f9a3ea2014a3bf5c4c0c653e4d9
SimonHelleman <hellemansimon@gmail.com>
2020-01-04 12:41:24 -0500

    INITIAL COMMIT - KICKOFF UWO
    INFITIE RECHARGE HUZZZZAA!!!

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,38 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;s
+
+public class Robot extends TimedRobot {
+ 
+  @Override
+  public void robotInit() {
+  }
+
+  @Override
+  public void robotPeriodic() {
+  }
+
+  @Override
+  public void autonomousInit() {
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+  }
+
+  @Override
+  public void teleopInit() {
+  }
+
+  @Override
+  public void teleopPeriodic() {
+  }
+
+  @Override
+  public void testInit() {
+  }
+
+  @Override
+  public void testPeriodic() {
+  }
+}
