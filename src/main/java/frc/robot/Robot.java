c07da958b6206791c4457916cf96a1ebbe374119
SimonHelleman <hellemansimon@gmail.com>
2020-01-04 14:45:56 -0500

    Robot Map & Colour Sensor

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,15 +1,22 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.TimedRobot;s
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.util.Color;
 
-public class Robot extends TimedRobot {
- 
+import com.revrobotics.ColorSensorV3;
+
+public class Robot extends TimedRobot implements RobotMap {
+
+  
+  ColorSensorV3 colourSensor = new ColorSensorV3(I2C_COLOUR_SENSOR);
+
   @Override
   public void robotInit() {
   }
 
   @Override
   public void robotPeriodic() {
+    
   }
 
   @Override
@@ -34,5 +41,9 @@ public class Robot extends TimedRobot {
 
   @Override
   public void testPeriodic() {
+  }
+
+  public Color getColourSensor(){
+      return colourSensor.getColor();
   }
 }
---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,47 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.I2C;
+
+public interface RobotMap {
+    /**
+     * ROBOT MAP
+     * 
+     * Constants for physical conections on robot
+     * 
+     * public static final int CONECTOR_LOCATION/SUBSYSTEM = NUMBER
+     * 
+     * to reference data from RobotMap include
+     * 
+     * "implements RobotMap" on the class declaration
+     * ex. Public class CoolRobot implements RobotMap
+     * 
+     * DO NOT HARDCODE NUMBERS IN CODE. ALWAYS UPDATE & REFERENCE THIS FILE. MAGIC NUMBERS SUCK!!!!
+     *  
+     */
+
+
+    /**
+     * Controllers:
+     * Number in drivestation
+     * NOTE:    Position in ds list can be locked inside drivestation
+     *          Make sure this is done before each match and check before each match 
+     *          that the order is correct.
+     */
+
+     //Driver Controller
+     public static final int CONTROLLER_DRIVER = 0;
+
+     /**
+      * Motor Controllers:
+      * Specify the connection type (CAN, PWM) and location/subsystem in the constant
+      */
+      public static final int CAN_LEFT_FRONT_DRIVE = 0;
+      public static final int CAN_RIGHT_FRONT_DRIVE = 1;
+      public static final int CAN_LEFT_BACK_DRIVE = 0;
+      public static final int CAN_RIGHT_BACK_DRIVE = 1;
+
+      /**
+       * Colour Sensor
+       */
+      public static final I2C.Port I2C_COLOUR_SENSOR = I2C.Port.kOnboard;
+}
