b1a8bb2b719915f9b06b562bc62181199fc015f0
SimonHelleman <hellemansimon@gmail.com>
2020-01-06 14:28:47 -0500

    Housekeeping - No Notable Changes

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,42 +1,39 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.I2C;
-
 public interface RobotMap {
     /**
      * ROBOT MAP
      * 
      * Constants for physical conections on robot
      * 
+     * FORMAT: (data type can be changed as needed)
      * public static final int CONECTOR_LOCATION/SUBSYSTEM = NUMBER
      * 
      * to reference data from RobotMap include
      * 
-     * "implements RobotMap" on the class declaration
-     * ex. Public class CoolRobot implements RobotMap
+     * "implements RobotMap" on the class declaration ex. Public class CoolRobot
+     * implements RobotMap
      * 
-     * DO NOT HARDCODE NUMBERS IN CODE. ALWAYS UPDATE & REFERENCE THIS FILE. MAGIC NUMBERS SUCK!!!!
-     *  
+     * DO NOT HARDCODE NUMBERS IN CODE. ALWAYS UPDATE & REFERENCE THIS FILE. 
+     * MAGIC NUMBERS SUCK!!!!
+     * 
      */
-
 
     /**
-     * Controllers:
-     * Number in drivestation
-     * NOTE:    Position in ds list can be locked inside drivestation
-     *          Make sure this is done before each match and check before each match 
-     *          that the order is correct.
+     * Controllers: Number in drivestation NOTE: Position in ds list can be locked
+     * inside drivestation Make sure this is done before each match and check before
+     * each match that the order is correct.
      */
 
-     //Driver Controller
-     public static final int CONTROLLER_DRIVER = 0;
+    // Driver Controller
+    public static final int CONTROLLER_DRIVER = 0;
 
-     /**
-      * Motor Controllers:
-      * Specify the connection type (CAN, PWM) and location/subsystem in the constant
-      */
-      public static final int CAN_LEFT_FRONT_DRIVE = 0;
-      public static final int CAN_RIGHT_FRONT_DRIVE = 1;
-      public static final int CAN_LEFT_BACK_DRIVE = 0;
-      public static final int CAN_RIGHT_BACK_DRIVE = 1;
+    /**
+     * Motor Controllers: Specify the connection type (CAN, PWM) and
+     * location/subsystem in the constant
+     */
+    public static final int CAN_LEFT_FRONT_DRIVE = 0;
+    public static final int CAN_RIGHT_FRONT_DRIVE = 1;
+    public static final int CAN_LEFT_BACK_DRIVE = 0;
+    public static final int CAN_RIGHT_BACK_DRIVE = 1;
 }
