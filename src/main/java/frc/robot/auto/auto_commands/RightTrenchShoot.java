80c03a6a05b6328496e1e60123fd9fd589489710
Tntcommando1 <tntcommando1@gmail.com>
2020-03-10 21:04:33 -0400

    Right side of target auto

--- src/main/java/frc/robot/auto/auto_commands/RightTrenchShoot.java
+++ src/main/java/frc/robot/auto/auto_commands/RightTrenchShoot.java
@@ -1,18 +1,23 @@
 package frc.robot.auto.auto_commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.drivetrain.DriveAngle;
+import frc.robot.commands.drivetrain.DriveDistance;
 import frc.robot.commands.drivetrain.ProfileFollower;
+import frc.robot.commands.drivetrain.ZeroGyro;
 import viking.ProfileBuffer;
 
-public class MiddleTrenchShoot extends SequentialCommandGroup {
-  
-  private static ProfileBuffer lineTrench = new ProfileBuffer("line-trench");
+public class RightTrenchShoot extends SequentialCommandGroup {
+
   private static ProfileBuffer ballShoot = new ProfileBuffer("ball-shoot");
 
-  public MiddleTrenchShoot() {
+  public RightTrenchShoot() {
     super(
-      new AimShoot().withTimeout(4), 
-      new ProfileFollower(lineTrench).raceWith(
+      new ZeroGyro(),
+      new DriveAngle(-20, 0.5).withTimeout(1.0),
+      new AimShoot().withTimeout(3.0),
+      new DriveAngle(0, 0.5).withTimeout(1.0),
+      new DriveDistance(5).raceWith(
         new RunConveyorTime(
           new double[][] {
             // After 1 second, drive both conveyors at 50% speed, then after 5 seconds stop
@@ -20,7 +25,7 @@ public class MiddleTrenchShoot extends SequentialCommandGroup {
             {5.0, 0.0, 0.0}
           }
         )
-      ), 
+      ),
       new ProfileFollower(ballShoot), 
       new AimShoot().withTimeout(4)
     );
