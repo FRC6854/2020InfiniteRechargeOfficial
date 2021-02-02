14a2afa9466a509154101686c56073e855aa4322
Collin Town <tntcommando1@gmail.com>
2021-02-02 12:41:30 -0500

    Updated Timings
    Last commit for 2020 Infinite Recharge hopefully.

--- src/main/java/frc/robot/auto/auto_commands/MiddleTrenchShoot.java
+++ src/main/java/frc/robot/auto/auto_commands/MiddleTrenchShoot.java
@@ -15,9 +15,8 @@ public class MiddleTrenchShoot extends SequentialCommandGroup {
       new ProfileFollower(lineTrench).raceWith(
         new RunConveyorTime(
           new double[][] {
-            // After 1 second, drive both conveyors at 50% speed, then after 5 seconds stop
-            {1.0, 0.5, 0.5},
-            {5.0, 0.0, 0.0}
+            {1.0, 1.0, 0.25},
+            {6.5, 0.0, 0.0}
           }
         )
       ), 
